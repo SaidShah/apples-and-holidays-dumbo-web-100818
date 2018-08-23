5be425a8bc50d79b7e961b34430ff19b190bbb62
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-23 00:20:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,12 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |name, holiday|
     holiday.each do |day, value|
-      value.each do |x|
-        if x == "BBQ"
-          return holiday.keys.flatten
-        end
-      end
-    end
+   
   end
 end
 
